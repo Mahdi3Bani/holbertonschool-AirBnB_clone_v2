6a06a6782cefe48819d5038a778b53873eba4e1a
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-30 15:28:54 +0100

    route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,9 +17,11 @@ def hello():
 def hi():
     return ('HBNB')
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def gm(text):
-    return ('C %s' %text.replace('_',' '))
+    return ('C %s' % text.replace('_', ' '))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
