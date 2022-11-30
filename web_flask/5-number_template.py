956e36bd43073a02c1fabad78f1fee9c8faae9a3
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-30 16:19:23 +0100

    route

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -10,32 +10,40 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """"hello flask"""
     return ('Hello HBNB!')
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hi():
+    """"hi hbnb"""
     return ('HBNB')
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def gm(text):
+    """"c is fun"""
     return ('C {}'.format(text.replace('_', ' ')))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pyth(text="is cool"):
+    """"python is cool"""
+
     return ('Python {}'.format(text.replace('_', ' ')))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def num(n):
+    """" number"""
+
     return ('{} is a number'.format(n))
 
 
 @app.route('/number_template/<int:n>>', strict_slashes=False)
 def temp(n):
+    """"template number"""
     return render_template('5-number.html', n=n)
 
 
