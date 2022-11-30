93cb55c40d2b8273a5fd77acecb151a51a8e56c5
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-30 13:04:20 +0100

    hello

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/',strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 if __name__ == 'main':
     app.run(host='0.0.0.0', port=5000)
