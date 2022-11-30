f7545382a52684bd2b2c8faa7c0f4e78159bfd90
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-30 15:58:05 +0100

    route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -10,33 +10,28 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """"hello"""
     return ('Hello HBNB!')
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hi():
-    """"hbnb"""
     return ('HBNB')
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def gm(text):
-    """"gm"""
     return ('C {}'.format(text.replace('_', ' ')))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pyth(text="is cool"):
-    """"pyth"""
     return ('Python {}'.format(text.replace('_', ' ')))
 
 
-@app.route('/number/<int: n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def num(n):
-    """"num"""
-    return '{} is a number'.format(n)
+    return ('{} is a number'.format(n))
 
 
 if __name__ == '__main__':
