4292bba15a0e8d64d011e7506ad2affcab5882c3
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-01 11:18:13 +0100

    route

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls == None:
+        if cls is None:
             return FileStorage.__objects
         new = {}
         for k, v in FileStorage.__objects.items():
@@ -57,7 +57,7 @@ class FileStorage:
 
     def delete(self, obj=None):
         """delete objects"""
-        if obj == None:
+        if obj is None:
             return
 
         key = obj.__class__.__name__ + '.' + obj.id
