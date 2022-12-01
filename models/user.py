2fd36b279c33a9ff739a0cc12c672670e5ae2d8b
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-01 11:34:17 +0100

    fixing

--- models/user.py
+++ models/user.py
@@ -3,14 +3,21 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models import storage_t
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship("Place",  backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", back_populates="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
